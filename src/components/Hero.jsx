a63588b8d5f2b5593724fdd9e3af6b752074c17c
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-07-08 13:43:55 +0600

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all';
 import { gsap } from 'gsap';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+    const videoRef = useRef();
+    const VideoTimelineRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type: ['chars', 'words']});
         const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -38,8 +43,34 @@ const Hero = () => {
         })
         .to('.right-leaf', { y: 200}, 0)
         .to('.left-leaf', { y: -200}, 0)
-    }, []);
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        // video animation timeline
+        VideoTimelineRef.current = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+
+                
+            }
+        });
+
+        if (videoRef.current) {
+            videoRef.current.onloadedmetadata = () => {
+                VideoTimelineRef.current.to(videoRef.current, {
+                    currentTime: videoRef.current.duration,
+                });
+            };
+        }
+    }, [isMobile]);
+
     return (
+        <>
         <section id='hero' className='noisy'>
             <h1 className='title'>MOJITO</h1>
 
@@ -62,6 +93,10 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+        <div className='video absolute inset-0'>
+            <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload='auto' />
+        </div>
+        </>
     )
 }
 
